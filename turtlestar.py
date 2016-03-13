b19073928e64228b4a08f639d0e8869a73ba159b
M. Andrew Moshier <moshier@chapman.edu>
2016-03-13 12:26:40 -0700

    Comments added by Dr. M

--- turtlestar.py
+++ turtlestar.py
@@ -4,7 +4,7 @@ side_len = int(input("Enter the length of each side:"))
 import turtle
 t = turtle.Pen()
 
-
+### MAM: This draws a regular polygon, not a star.
 def draw_star(t,num_sides,side_len):
     for i in range (num_sides):
         t.forward(side_len)
@@ -16,10 +16,12 @@ bob.goto(-side_len/2,0)
 bob.pendown()
 
 for j in range (num_sides):
-    draw_star(bob,1,side_len)
+    draw_star(bob,1,side_len)  ##$ MAM: Draw a one-sides regular polygon? Weird.
     bob.left(180-(180/num_sides))
 
 
+### MAM: You are using puython 3. This makes it complicated for me to debug,
+### because this line will crash for me. (I have to replace 'input' with 'raw_input'
 stopper = input("Hit <enter> to quit.")
 
 turtle.bye()
